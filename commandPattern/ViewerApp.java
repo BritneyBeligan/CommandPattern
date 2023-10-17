db530b68c5773b3f0c69366cc04f41cb54d4f6ce
Britney <142373706+BritneyBeligan@users.noreply.github.com>
2023-10-17 22:56:41 +0800

    Update ViewerApp.java

--- commandPattern/ViewerApp.java
+++ commandPattern/ViewerApp.java
@@ -15,6 +15,17 @@ public class ViewerApp {
         System.out.println(powerOff.execute());
 
 
+        // Features of Smart Tv : Stream Music
+        ListenMusic listenMusic = new ListenMusic(tv);
+        System.out.println(listenMusic.execute());
+
+        // Features of Smart Tv : Play Smart Fun Games
+        SmartGames smartGames = new SmartGames(tv);
+        System.out.println(smartGames.execute());
+        
+        // Features of Smart Tv : Split Screen
+        MultiView multiview = new MultiView(tv);
+        System.out.println(multiview.execute());
 
     }
 }
