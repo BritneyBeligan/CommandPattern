8dfb014f3d635535f13bc22d52aae29374083e71
Britney <142373706+BritneyBeligan@users.noreply.github.com>
2023-10-17 22:53:21 +0800

    Update Tv.java

--- commandPattern/Tv.java
+++ commandPattern/Tv.java
@@ -10,4 +10,18 @@ public class Tv {
     public String switchOff(){
         return "Tv is switched-off!";
     }
+
+    //---
+    public String switchMusic(){
+        return "Music is Activated! Choose your favorite song and stream online now!";
+    }
+
+    //---
+    public String switchGames(){
+        return"Do you want to play Fun Smart Tv Games? Play Now!";
+    }
+
+    public String switchView(){
+        return"MultiView: Click this Button to split up to 2 screens";
+    }
 }
